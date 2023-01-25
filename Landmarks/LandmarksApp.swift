d1ecfcdc0e5c7e671b6bfc1dabd0e22c89418657
Stephen Wool <stevewool7@gmail.com>
2023-01-24 21:28:53 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Stephen Wool on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
